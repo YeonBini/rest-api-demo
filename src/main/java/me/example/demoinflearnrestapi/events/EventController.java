ac58010a5e9ed08bc392bb3ca6f6f036159933a9
정연빈 <yeonbn88@gmail.com>
2019-06-11 00:27:31 +0900

    [19.06.10] bad reqeust

--- src/main/java/me/example/demoinflearnrestapi/events/EventController.java
+++ src/main/java/me/example/demoinflearnrestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package me.example.demoinflearnrestapi.events;
 
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @AllArgsConstructor
 @Controller
@@ -20,8 +20,11 @@ public class EventController {
 
     EventRepository eventRepository;
 
+    ModelMapper modelMapper;
+
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto, ModelMapper modelMapper) {
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
--- src/test/java/me/example/demoinflearnrestapi/events/EventControllerTest.java
+++ src/test/java/me/example/demoinflearnrestapi/events/EventControllerTest.java
@@ -1,11 +1,14 @@
 package me.example.demoinflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -20,7 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+//@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
 
     @Autowired
@@ -33,11 +38,45 @@ public class EventControllerTest {
      * WebMvcTest의 slice test에서는 Repository Bean 생성이 되어 있지 않아
      * 별도의 MockBean을 등록해주어야 한다.
      */
-    @MockBean
-    EventRepository eventRepository;
+//    @MockBean
+//    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
+
+        // Given
+        EventDto event = EventDto.builder()
+                        .name("Spring")
+                        .description("REST api lecture")
+                        .beginEnrollmentDateTime(LocalDateTime.of(2019, 6, 9, 15, 16))
+                        .closeEnrollmentDateTime(LocalDateTime.of(2019, 6, 10, 0, 0))
+                        .beginEventDateTime(LocalDateTime.of(2019, 6, 9, 15, 16))
+                        .endEventDateTime(LocalDateTime.of(2019, 6, 10, 0, 0))
+                        .basePrice(100)
+                        .maxPrice(200)
+                        .location("왕십리역 삼부아파트")
+                        .build();
+
+//        Mockito.when(eventRepository.save(event))
+//                .thenReturn(event);
+
+        // Then
+        mockMvc.perform(post("/api/events")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .accept(MediaTypes.HAL_JSON)
+                            .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(10)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)));
+    }
+
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
 
         // Given
         Event event = Event.builder()
@@ -51,11 +90,10 @@ public class EventControllerTest {
                         .basePrice(100)
                         .maxPrice(200)
                         .limitOfEnrollment(100)
-                        .location("왕십리역 삼부아파")
+                        .location("왕십리역 삼부아파트")
+                        .free(true)
                         .build();
 
-        Mockito.when(eventRepository.save(event))
-                .thenReturn(event);
 
         // Then
         mockMvc.perform(post("/api/events")
@@ -63,12 +101,8 @@ public class EventControllerTest {
                             .accept(MediaTypes.HAL_JSON)
                             .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE));
-//                .andExpect(header().exists("Location"))
-//                .andExpect(header().string("Content-Type", "application/hal+json;charset=UTF-8"));
+                .andExpect(status().isBadRequest())
+        ;
     }
 
 }
